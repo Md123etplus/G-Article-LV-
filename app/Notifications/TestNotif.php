3f3597469b6e1e4278521febd6eca6d204655e51
Moussa Dembele <134782079+Md123etplus@users.noreply.github.com>
2024-06-07 03:43:05 +0100

    B4 merge

--- app/Notifications/TestNotif.php
+++ app/Notifications/TestNotif.php
@@ -26,7 +26,7 @@ class TestNotif extends Notification
      */
     public function via(object $notifiable): array
     {
-        return ['mail'];
+        return ['mail','database'];
     }
 
     /**
@@ -48,7 +48,7 @@ class TestNotif extends Notification
     public function toArray(object $notifiable): array
     {
         return [
-            //
+            "title"=>'This is the title'
         ];
     }
 }
